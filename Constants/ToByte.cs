4986833b83d55a4f5c7e6f9e094ac8e8d786c4bd
MagicNumber <~>
2010-11-26 23:37:22 +0000

    Missed a file

--- Constants/ToByte.cs
+++ Constants/ToByte.cs
@@ -10,8 +10,8 @@ namespace SpacecraftGT.Constants
         static byte[] ToBytes(this string S)
         {
             var B = new Builder<Byte>();
-            B.Append((byte) (S.Length / 255));
-            B.Append((byte) (S.Length % 256));
+            B.Append((byte)(UTF8Encoding.UTF8.GetByteCount(S) / 255));
+            B.Append((byte)(UTF8Encoding.UTF8.GetByteCount(S) % 256));
             B.Append(UTF8Encoding.UTF8.GetBytes(S));
             return B.ToArray();
         }

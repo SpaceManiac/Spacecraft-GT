f57294da1a65f667fce0c9ad8ebb8b3d12e33112
MagicNumber <~>
2010-11-27 11:44:19 +0000

    Added short-to-bytes

--- Constants/ToByte.cs
+++ Constants/ToByte.cs
@@ -7,11 +7,21 @@ namespace SpacecraftGT.Constants
 {
     static class ByteClass
     {
+
+        static byte[] ToBytes(this short S)
+        {
+            // Big-Endian because that's what Minecraft uses. 
+            return new byte[] { 
+                (byte)(S / 256), 
+                (byte)(S % 256) 
+            };
+        }
+
         static byte[] ToBytes(this string S)
         {
+            short len = (short) UTF8Encoding.UTF8.GetByteCount(S);
             var B = new Builder<Byte>();
-            B.Append((byte)(UTF8Encoding.UTF8.GetByteCount(S) / 255));
-            B.Append((byte)(UTF8Encoding.UTF8.GetByteCount(S) % 256));
+            B.Append(len.ToBytes());
             B.Append(UTF8Encoding.UTF8.GetBytes(S));
             return B.ToArray();
         }

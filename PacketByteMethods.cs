2a5d8ee9b623e0c17561e6619e729c8c2236c0bd
MagicNumber <~>
2010-11-28 18:08:46 +0000

    Getting rid of literals

--- PacketByteMethods.cs
+++ PacketByteMethods.cs
@@ -17,45 +17,53 @@ namespace SpacecraftGT
         private static short GetNextShort(byte[] data, ref int Index)
         {
             short S = BitConverter.ToInt16(data, Index);
-            Index += 2;
+            Index += sizeof(short);
             return S;
         }
 
         private static int GetNextInt(byte[] data, ref int Index)
         {
             int I = BitConverter.ToInt32(data, Index);
-            Index += 4;
+            Index += sizeof(int);
             return I;
         }
 
         private static long GetNextLong(byte[] data, ref int Index)
         {
-            return 0;
+            long value = BitConverter.ToInt64(data, Index);
+            Index += sizeof(long);
+            return value;
         }
 
         private static float GetNextFloat(byte[] data, ref int Index)
         {
-            return 0;
+            float value = BitConverter.ToSingle(data, Index);
+            Index += sizeof(float);
+            return value;
         }
 
         private static bool GetNextBool(byte[] data, ref int Index)
         {
-            throw new NotImplementedException();
+            byte b = data[Index++];
+            return (b == 1);
         }
 
         private static double GetNextDouble(byte[] data, ref int Index)
         {
-            throw new NotImplementedException();
+            double value = BitConverter.ToDouble(data, Index);
+            Index += sizeof(double);
+            return value;
         }
 
         private static Byte GetNextByte(byte[] data, ref int Index)
         {
-            throw new NotImplementedException();
+            return data[Index++];
         }
 
         private static Byte[] GetNextByteArray(byte[] data, ref int Index)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            int f = sizeof(HandshakePacket);
         }
 
     }
